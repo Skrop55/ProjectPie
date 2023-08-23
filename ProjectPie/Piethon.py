e1256d60b0a42e66d545f35bab75017a1b86c153
Skrop <116880776+Skrop55@users.noreply.github.com>
2023-08-23 12:34:59 +0300

    Add files via upload

---
+++ ProjectPie/Piethon.py
@@ -0,0 +1,5 @@
+print("Hi there! \n"
+      "I'm learning python rn, and I think that python sound like \'pie eating marathon\', therefore the file name is:")
+pie = "Pie"
+marathon = "thon"
+print(pie + marathon, "xD")
